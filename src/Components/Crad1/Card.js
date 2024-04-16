c03347e0767bca2e4128badc63ae25badcb52148
saurabh <saurabhbhatti1310@gmail.com>
2024-04-16 18:22:37 +0530

    Change the layout of Team section.

--- src/Components/Crad1/Card.js
+++ src/Components/Crad1/Card.js
@@ -1,72 +1,73 @@
 import React from "react";
-import img1 from "../../Assets/Image/img1.jpg";
-import img2 from "../../Assets/Image/img2.jpg";
-import img3 from "../../Assets/Image/img3.jpg";
-import img4 from "../../Assets/Image/img4.jpg";
 import "../Crad1/Card.css";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import doct1 from "../../Assets/Image/dct1.jpg";
+import doct2 from "../../Assets/Image/dct2.jpg";
+import doct3 from "../../Assets/Image/dct3.jpg";
 
 const Card = () => {
   return (
-    <div className="container">
-      <div className="row cards">
-        <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
-          <div className="card">
-            <img src={img1} alt="img1" className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">DR. Prateek Mehta</h5>
-              <p className="card-text">
-                I'm a paragraph. Click here to add your own text and edit me.
-                Let your users get to know you.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
-          <div className="card">
-            <img src={img2} alt="img2" className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">Evelyn Wright, M.D</h5>
-              <p className="card-text">
-                I'm a paragraph. Click here to add your own text and edit me.
-                Let your users get to know you.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
-          <div className="card">
-            <img src={img3} alt="img3" className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">Jenna Cadwell, M.D</h5>
-              <p className="card-text">
-                I'm a paragraph. Click here to add your own text and edit me.
-                Let your users get to know you.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
-          <div className="card">
-            <img src={img4} alt="img4" className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">David Wayne, M.D.</h5>
-              <p className="card-text">
-                I'm a paragraph. Click here to add your own text and edit me.
-                Let your users get to know you.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div style={{ height: 'auto', width: '100%' }}>
-          <h2 style={{ fontSize: '56px', textAlign: 'center' }}>
-            <span style={{ fontSize: '56px', color:'rgb(9,55,31)' }}>
-              Our Services
-            </span>
-          </h2>
-        </div>
-        <div>
-        </div>
-      </div>
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          alignContent: "center",
+          justifyContent: "center",
+          "& > :not(style)": {
+            m: 1,
+            width: 280,
+            height: 390,
+          },
+        }}
+      >
+        <Paper elevation={3} sx={{ border: "1px solid gray" }}>
+          <Box>
+            <img src={doct1} />
+            <Typography sx={{ marginTop: "4px" }}>
+              Jaimee Ussery – Nurse Practitioner
+            </Typography>
+            <Typography sx={{ fontSize: 12, marginTop: "8px" }}>
+              I'm a paragraph. Click here to add your own text and edit me. Let
+              your users get to know you.
+            </Typography>
+            <Button sx={{ marginTop: "8px" }} variant="text">
+              Book Now
+            </Button>
+          </Box>
+        </Paper>
+
+        <Paper elevation={3} sx={{ border: "1px solid gray" }}>
+          <Box>
+            <img src={doct2} />
+            <Typography sx={{ marginTop: "4px" }}>
+              Sandrine Ndetah – Nurse Practitioner
+            </Typography>
+            <Typography sx={{ fontSize: 12, marginTop: "8px" }}>
+              I'm a paragraph. Click here to add your own text and edit me. Let
+              your users get to know you.
+            </Typography>
+            <Button sx={{ marginTop: "8px" }} variant="text">
+              Book Now
+            </Button>
+          </Box>
+        </Paper>
+        <Paper elevation={3} sx={{ border: "1px solid gray" }}>
+          <Box>
+            <img src={doct3} />
+            <Typography sx={{ marginTop: "4px" }}>
+              Dawn Coston – Nurse Practitioner
+            </Typography>
+            <Typography sx={{ fontSize: 12, marginTop: "8px" }}>
+              I'm a paragraph. Click here to add your own text and edit me. Let
+              your users get to know you.
+            </Typography>
+            <Button sx={{ marginTop: "8px" }} variant="text">
+              Book Now
+            </Button>
+          </Box>
+        </Paper>
+      </Box>
     </div>
   );
 };
