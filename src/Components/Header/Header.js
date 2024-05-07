ace433df4abd0e1b73d1fd138077e9fd1b7324da
saurabh <saurabhbhatti1310@gmail.com>
2024-05-07 16:37:44 +0530

    fix-bug hamburger menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -19,36 +19,36 @@ class NavBar extends React.Component {
   }
 
   render() {
-    const { showMenu } = this.state;
+  const { showMenu } = this.state;
 
-    return (
-      <>
-        <p className='disclaimer'>
-          Telehealth weight loss treatment for a Healthier, Happier You
-        </p>
-        <header className="navbar">
-          <div className="logo-container">
-            <img src={logo} alt="Your Logo" className="logo" />
+  return (
+    <>
+      <p className='disclaimer'>
+        Telehealth weight loss treatment for a Healthier, Happier You
+      </p>
+      <header className="navbar">
+        <div className="logo-container">
+          <img src={logo} alt="Your Logo" className="logo" />
+        </div>
+        <ul className={`nav-links ${showMenu ? 'show' : ''}`}>
+          <li><Link to='/home' onClick={this.toggleMenu}>Home</Link></li>
+          <li><Link to='/howitworks' onClick={this.toggleMenu}>How it Works</Link></li>
+          <li><Link to="/appointments" onClick={this.toggleMenu}>Book a Session</Link></li>
+          <li><Link to="/otcteam" onClick={this.toggleMenu}>OTC Team</Link></li>
+          <li><Link to="/refills" onClick={this.toggleMenu}>Refills</Link></li>
+          <li><Link to="/pricing" onClick={this.toggleMenu}>Pricing</Link></li>
+        </ul>
+        <div className="menu-toggle" onClick={this.toggleMenu}>
+          <div className={`hamburger-icon ${showMenu ? 'open' : ''}`}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
           </div>
-          <div className="menu-toggle" onClick={this.toggleMenu}>
-            <div className={`hamburger-icon ${showMenu ? 'open' : ''}`}>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-            </div>  
-          </div>
-          <ul className={`nav-links ${showMenu ? 'show' : ''}`}>
-            <li><Link to='/home' onClick={this.toggleMenu}>Home</Link></li>
-            <li><Link to='/howitworks' onClick={this.toggleMenu}>How it Works</Link></li>
-            <li><Link to="/appointments" onClick={this.toggleMenu}>Book a Session</Link></li>
-            <li><Link to="/otcteam" onClick={this.toggleMenu}>OTC Team</Link></li>
-            <li><Link to="/refills" onClick={this.toggleMenu}>Refills</Link></li>
-            <li><Link to="/pricing" onClick={this.toggleMenu}>Pricing</Link></li>
-          </ul>
-        </header>
-      </>
-    );
-  }
+        </div>
+      </header>
+    </>
+  );
+}
 }
 
 export default NavBar;
